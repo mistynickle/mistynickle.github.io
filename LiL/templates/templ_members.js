0c7032b36265d5f96ca35c323f2f92e4291bdb49
Seph Reed <SephReed@Sephs-MacBook-Pro.local>
2018-01-11 21:19:12 -0600

    better images

--- LiL/templates/templ_members.js
+++ LiL/templates/templ_members.js
@@ -3,16 +3,18 @@ var Members = [
 		name: "Misty Nickle",
 		img: "../../public/images/Sophie_Misty.jpeg",
 		info: [
-			{ name: "Bio", data: `It's Bebe` },
+			// { name: "Quote", data: `` },
+			{ name: "Bio", data: `Awesome Person` },
 			{ name: "Portfolio", data: `<a href="https://mistynickle.github.io/index.html">MistyNickle.github.io</a>` },
 			{ name: "Hails From", data: `Seattle WA -> L.A. CA -> Austin TX` },
 		],
 	},
 	{
 		name: "Seph Reed",
-		img: "../../public/images/Sophie_Misty.jpeg",
+		img: "../../public/images/photos/Seph.jpg",
 		info: [
-			{ name: "Bio", data: `It's Bebe` },
+			{ name: "Quote", data: `Everything is a Paradox` },
+			{ name: "Bio", data: `Builder, Coder` },
 			{ name: "Portfolio", data: `<a href="https://sephreed.github.io/portfolio/index.html">SephReed.github.io/portfolio/</a>` },
 			{ name: "Hails From", data: `Santa Cruz CA -> Portland OR -> Austin TX` },
 		],
